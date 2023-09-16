c347c6df7e47658109112c1027349ed5eb3aa5b3
youssef-m-roushdy <yossefmahmoud442004@gmail.com>
2023-09-16 10:22:02 -0700

    my printf

--- _printf.c
+++ _printf.c
@@ -1,81 +1,59 @@
 #include <stdarg.h>
 #include <stdio.h>
+#include <string.h>
+#include "main.h"
 
+int _printf(const char *format, ...) {
+va_list args;
+int lenOfString = 0;
 
-int _printf(const char *format, ...)
+va_start(args, format);
+while (*format)
 {
-    int len;
-    va_list args;
-    va_start(args, format);
-
-    len = 0;
-    while (*format) {
-        if (*format == '%') {
-            format++;
-            if (*format == 'd' || *format == 'i')
-            {
-                printf("%d", va_arg(args, int));
-                len++;
-            }
-            else if(*format == 'u')
-            {
-                printf("%u", va_arg(args, unsigned int));
-                len++;
-            }
-            else if(*format == 'o')
-            {
-                printf("%o", va_arg(args, unsigned int));
-                len++;
-            }
-            else if(*format == 'x')
-            {
-                printf("%x", va_arg(args, unsigned int));
-                len++;
-            }
-            else if(*format == 'X')
-            {
-                printf("%X", va_arg(args, unsigned int));
-                len++;
-            }
-            else if(*format == 'p')
-            {
-                printf("%p", va_arg(args, void *));
-                len++;
-            }
-            else if (*format == 's')
-            {
-                printf("%s", va_arg(args, char *));
-                len++;
-            }
-            else if (*format == 'c')
-            {
-                putchar(va_arg(args, int));
-                len++;
-            }
-            else if (*format == 'c')
-            {
-                putchar(va_arg(args, int));
-                len++;
-            }
-	    else if (*format == 'r')
-            {
-                printf("%r");
-                len++;
-            }
-            else
-            {
-                putchar(*format);
-                len++;
-            }
-        }
-        else
-        {
-            putchar(*format);
-            len++;
-        }
-        format++;
-    }
-
-    va_end(args);
-    return len;
+if (*format == '%')
+{
+format++;
+if (*format == '\0')
+{
+break;
+}
+if (*format == 'c')
+{
+char c = va_arg(args, int);
+putchar(c);
+lenOfString++;
+}
+else if (*format == 's')
+{
+char *str = va_arg(args, char *);
+if (str != NULL) {
+fputs(str, stdout);
+lenOfString += strlen(str);
+}
+}
+else if (*format == 'd' || *format == 'i')
+{
+int num = va_arg(args, int);
+printf("%d", num);
+lenOfString += snprintf(NULL, 0, "%d", num);
+}
+else if (*format == '%') {
+putchar('%');
+lenOfString++;
+}
+else
+{
+putchar(*format);
+lenOfString++;
+}
+}
+else
+{
+putchar(*format);
+lenOfString++;
+}
+format++;
+}
+va_end(args);
+return lenOfString;
 }
